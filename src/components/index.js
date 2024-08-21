4f2c397360b4aff609e2062e688354e1965a73af
Vasilisa <akanissa18@mail.ru>
2024-08-21 14:37:36 +0300

    Коммит после 1 итерации

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,27 @@ import '../pages/index.css';
 import { openModal, closeModal } from './modal.js';
 import { likeButton, deleteCard, createCard } from './card.js';
 
+// Переменные
+
+const popups = document.querySelectorAll('.popup');
+const buttonEditProfile = document.querySelector('.profile__edit-button');
+const profilePopup = document.querySelector('.popup_type_edit');
+const buttonsClosePopup = document.querySelectorAll('.popup__close');
+const buttonAddProfile = document.querySelector('.profile__add-button');
+const newCardPopup = document.querySelector('.popup_type_new-card');
+const popupsContent = document.querySelectorAll('.popup__content');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_description');
+const formEditProfile = document.forms['edit-profile'];
+const cardNameInput = document.querySelector('.popup__input_type_card-name');
+const cardLinkInput = document.querySelector('.popup__input_type_url');
+const formAddProfile = document.forms['new-place'];
+const popupImage = document.querySelector('.popup_type_image');
+const nameTitle = document.querySelector('.profile__title');
+const jobTitle = document.querySelector('.profile__description');
+const image = document.querySelector('.popup__image');
+const name = document.querySelector('.popup__caption');
+
 // Темплейт карточки
 
 export const container = document.querySelector('#card-template').content;
@@ -18,27 +39,12 @@ initialCards.forEach(function(item) {
   cardList.append(card);
 });
 
-// Переменные
-
-const popup = document.querySelectorAll('.popup');
-const buttonEditProfile = document.querySelector('.profile__edit-button');
-const profilePopup = document.querySelector('.popup_type_edit');
-const buttonClosePopup = document.querySelectorAll('.popup__close');
-const buttonAddProfile = document.querySelector('.profile__add-button');
-const newCardPopup = document.querySelector('.popup_type_new-card');
-const popupContent = document.querySelectorAll('.popup__content');
-const nameInput = document.querySelector('.popup__input_type_name');
-const jobInput = document.querySelector('.popup__input_type_description');
-const formEditProfile = document.forms['edit-profile'];
-const cardNameInput = document.querySelector('.popup__input_type_card-name');
-const cardLinkInput = document.querySelector('.popup__input_type_url');
-const formAddProfile = document.forms['new-place'];
-const popupImage = document.querySelector('.popup_type_image');
-
 // Слушатели открытия модального окна
 
 buttonEditProfile.addEventListener('click', function() {
   openModal(profilePopup);
+  nameInput.value = nameTitle.textContent;
+  jobInput.value = jobTitle.textContent;
 });
 
 buttonAddProfile.addEventListener('click', function() {
@@ -47,7 +53,7 @@ buttonAddProfile.addEventListener('click', function() {
 
 // Слушатели закрытия модального окна
 
-buttonClosePopup.forEach(function(item) {
+buttonsClosePopup.forEach(function(item) {
   const popup = item.closest('.popup');
   item.addEventListener('click', function() {
     closeModal(popup);
@@ -64,7 +70,7 @@ newCardPopup.addEventListener('click', function() {
 
 // Закрытие модального окна при нажатии на оверлей
 
-popup.forEach(function(overlay) {
+popups.forEach(function(overlay) {
   overlay.addEventListener('click', function(evt) {
     if (evt.target === overlay) {
       closeModal(overlay);
@@ -72,7 +78,7 @@ popup.forEach(function(overlay) {
   });
 });
 
-popupContent.forEach(function(item) {
+popupsContent.forEach(function(item) {
   const popup = item.closest('.popup__content');
   item.addEventListener('click', function(evt) {
     evt.stopPropagation();
@@ -86,9 +92,6 @@ function handleFormEditProfile(evt) {
 
   const nameValue = nameInput.value;
   const jobValue = jobInput.value; // Получите значение полей jobInput и nameInput из свойства value
-
-  const nameTitle = document.querySelector('.profile__title'); // Выберите элементы, куда должны быть вставлены значения полей
-  const jobTitle = document.querySelector('.profile__description'); 
     
   nameTitle.textContent = nameValue; // Вставьте новые значения с помощью textContent
   jobTitle.textContent = jobValue;
@@ -124,8 +127,6 @@ formAddProfile.addEventListener('submit', handleFormAddCard);
 // Открытие модального окна с картинкой
 
 function openPopupImage(item) {
-  const image = document.querySelector('.popup__image');
-  const name = document.querySelector('.popup__caption');
 
   name.textContent = item.name;
   image.src = item.link;
