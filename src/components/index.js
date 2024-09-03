2af110ee31277304e043d65859738365a2c7d57d
Vasilisa <akanissa18@mail.ru>
2024-09-03 11:15:50 +0300

    коммит с неработающей валидацией

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,7 @@ import { likeButton, deleteCard, createCard } from './card.js';
 // Переменные
 
 const popups = document.querySelectorAll('.popup');
+const popupForm = document.querySelector('.popup__form');
 const buttonEditProfile = document.querySelector('.profile__edit-button');
 const profilePopup = document.querySelector('.popup_type_edit');
 const buttonsClosePopup = document.querySelectorAll('.popup__close');
@@ -134,3 +135,96 @@ function openPopupImage(item) {
 
   openModal(popupImage);
 }
+
+______________________________________________________________________________________
+
+
+
+
+
+
+// Валидация формы
+
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}; 
+
+// Показать ошибку ввода
+
+const showInputError = function(form, input, errorMessage) {
+  const error = form.querySelector(`.${input.id}__input-error`);
+  input.classList.add(validationConfig.inputErrorClass);
+  error.textContent = errorMessage;
+  error.classList.add(validationConfig.errorClass);
+};
+
+// Спрятать ошибку ввода
+
+const hideInputError = function(form, input) {
+  const error = form.querySelector(`.${input.id}__input-error`);
+  input.classList.remove(validationConfig.inputErrorClass);
+  error.classList.remove(validationConfig.errorClass);
+  error.textContent = '';
+};
+
+// Проверить инпут на валидность
+
+const isValid = function(form, input) {
+  if (input.validity.patterMismatch) {
+    input.setCustomValidity(input.dataset.error);
+  } else {
+    input.setCustomValidity('');
+  }
+  if (!input.validity.valid) {
+    showInputError(form, input, input.validationMessage);
+  } else {
+    hideInputError(form, input);
+  }
+};
+
+// Проверить невалидность одного из инпута
+
+const hasInvalidInput = function(inputList) {
+  return inputList.some(function(input) {
+    return !input.validity.valid;
+  })
+};
+
+// Изменить кликабельность кнопки
+
+const toggleButtonState = function(inputList, button) {
+  if (hasInvalidInput(inputList)) {
+    button.disabled = true;
+    button.classList.add('button__inactive');
+  } else {
+    button.disabled = false;
+    button.classList.remove('button__inactive');
+  }
+};
+
+// Слушатели событий инпутов
+
+const setEventListeners = function(form) {
+  const inputList = Array.from(form.querySelectorAll(validationConfig.inputSelector));
+  const button = form.querySelector(validationConfig.submitButtonSelector);
+  inputList.forEach(function(input) {
+    input.addEventListener('input', function() {
+      isValid(form, input);
+      toggleButtonState(inputList, button);
+    });
+  });
+};
+
+const enableValidation = function() {
+  const formList = Array.from(document.querySelectorAll(validationConfig.formSelector));
+  formList.forEach(function(form) {
+    setEventListeners(form);
+  });
+};
+
+enableValidation();
